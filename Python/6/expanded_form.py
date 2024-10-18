75aac2aa15002761cb1203c6a52191d00a6052b8
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2024-10-18 22:04:47 +0500

    Daily Challenge. Day 1.

---
+++ Python/6/expanded_form.py
@@ -0,0 +1,14 @@
+def expanded_form(num: int):
+    nums = [int(i) for i in str(num)]
+    length = len(nums)
+
+    result = [str(n * 10 ** (length - idx - 1)) \
+              for idx, n in enumerate(nums) \
+                   if n != 0]
+
+    return ' + '.join(result)
+
+
+
+if __name__ == '__main__':
+    print(expanded_form(70304))
